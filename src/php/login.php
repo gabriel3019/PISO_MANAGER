8600ea6168b55ef03977c7dd84fb02c01ab6add0
Gabriel <gabrielbr190@gmail.com>
2026-06-02 18:03:16 +0200

    login.php

--- src/php/login.php
+++ src/php/login.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once __DIR__ . "/BBDD/crear_tabla.php";
+//require_once __DIR__ . "/BBDD/crear_tabla.php";
 require_once __DIR__ . "/BBDD/conecta.php";
 
 header('Content-Type: application/json');
