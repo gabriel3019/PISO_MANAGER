c45b1c4bab0ba74c51fefc85404ec3d6109c69d1
Gabriel <gabrielbr190@gmail.com>
2026-06-02 14:00:27 +0200

    modo oscuro arreglado

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -4,13 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
   aplicarModoOscuro();
 });
 
-/* ================= APLICAR ================= */
 function aplicarModoOscuro() {
   const dark = localStorage.getItem("darkMode") === "true";
   document.body.classList.toggle("dark", dark);
 }
 
-/* ================= CAMBIAR (OPCIONAL GLOBAL) ================= */
 function setModoOscuro(activo) {
   localStorage.setItem("darkMode", activo);
   document.body.classList.toggle("dark", activo);
