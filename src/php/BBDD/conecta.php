3ed86235d04630f11c145c2285b9dda1dbdeaf35
Gabriel <gabrielbr190@gmail.com>
2026-06-02 20:12:02 +0200

    conecta servidor

--- src/php/BBDD/conecta.php
+++ src/php/BBDD/conecta.php
@@ -1,22 +1,20 @@
 <?php
 
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "piso_manager";
+$servername = "sql203.infinityfree.com";
+$username   = "if0_42079927";
+$password   = "PisoManager2026";
+$dbname     = "if0_42079927_piso_manager";
 
-$conn = new mysqli(
-    $servername,
-    $username,
-    $password,
-    $dbname
-);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die(
-        "Error de conexión: " .
-        $conn->connect_error
-    );
+    header('Content-Type: application/json');
+    echo json_encode([
+        'success' => false,
+        'message' => 'Error de conexión con la base de datos: ' . $conn->connect_error
+    ]);
+    exit;
 }
 
 $conn->set_charset("utf8mb4");
+?>
